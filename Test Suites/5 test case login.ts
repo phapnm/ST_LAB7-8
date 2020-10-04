ecd22f326d49aaf931196bc2cc3574becd5e62a1
phapnm <phapnm>
2020-10-04 15:01:37 +0700

    Init

---
+++ Test Suites/5 test case login.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>5 test case login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d1228267-fa4c-4506-a11d-15ef50cac71e</testSuiteGuid>
+   <testCaseLink>
+      <guid>7a24c5f3-e90b-481c-83af-75ab7ec41b8b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/empty</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>14b23a4d-2291-4f19-8435-252bf22a2f76</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/id_false pw_false</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>fe50e6cb-76fb-4905-96a9-ce714577441b</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>32fcc691-5661-4c3f-9f74-746268904c1c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/id_false pw_true</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>43cd2bad-8cf8-4925-a93c-01fd5174fb3d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/id_true pw_false</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2de60058-f5ac-4b12-99ef-7ab361ac4b76</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/success</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
